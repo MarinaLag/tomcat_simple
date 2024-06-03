f8043438ce0a34aaf644559d0c7d7f423ad1c0af
Marina Lagoikina <marinalagoikina@gmail.com>
2024-06-03 20:24:58 +0300

    second commit

--- src/main/java/ru/aston/tomcat/servlet/HelloServlet.java
+++ src/main/java/ru/aston/tomcat/servlet/HelloServlet.java
@@ -1,4 +1,20 @@
 package ru.aston.tomcat.servlet;
 
-public class HelloServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello for servlet!");
+        printWriter.close();
+    }
 }
